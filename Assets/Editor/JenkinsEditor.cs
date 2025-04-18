a82c62f171b9b7d39ebfd0374aac924f5ee2ddfa
Weminsoo <weminsoo@naver.com>
2025-04-19 03:26:33 +0900

    젠킨스 에디터 수정
    스크립트 내용 수정

--- Assets/Editor/JenkinsEditor.cs
+++ Assets/Editor/JenkinsEditor.cs
@@ -1,33 +1,52 @@
 using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
+using System;
+using System.IO;
 
-class MyEditorScript
+class JenkinsEditor
 {
     [MenuItem("Build/Perform Android Build")]
     static void PerformAndroidBuild()
     {
-        // 1) Android를 활성 빌드 타겟으로 전환 (선택 사항이지만 권장)
+        // 1) Android 플랫폼 전환
         EditorUserBuildSettings.SwitchActiveBuildTarget(
-            BuildTargetGroup.Android, BuildTarget.Android);
+            BuildTargetGroup.Android,
+            BuildTarget.Android);
 
-        // 2) 빌드할 씬 목록 가져오기
+        // 2) 빌드할 씬 목록 확보
         string[] scenes = FindEnabledEditorScenes();
 
-        // 3) APK로 빌드
+        // 3) Jenkins WORKSPACE 환경변수에서 베이스 경로 가져오기
+        string workspace = Environment.GetEnvironmentVariable("WORKSPACE");
+        string basePath = !string.IsNullOrEmpty(workspace)
+            ? workspace
+            : Path.GetFullPath(Path.Combine(Application.dataPath, ".."));
+
+        // 4) 빌드 출력 경로 설정
+        string buildDir = Path.Combine(basePath, "Builds", "Android");
+        Directory.CreateDirectory(buildDir);
+        string buildPath = Path.Combine(buildDir, "MyGame.apk");
+
+        // 5) APK 빌드 실행
         BuildPipeline.BuildPlayer(
             scenes,
-            "Builds/Android/MyGame.apk",
+            buildPath,
             BuildTarget.Android,
-            BuildOptions.None);
+            BuildOptions.None
+        );
+
+        Debug.Log($"[JenkinsEditor] Build completed at: {buildPath}");
     }
 
     private static string[] FindEnabledEditorScenes()
     {
         var editorScenes = new List<string>();
         foreach (var scene in EditorBuildSettings.scenes)
+        {
             if (scene.enabled)
                 editorScenes.Add(scene.path);
+        }
         return editorScenes.ToArray();
     }
 }

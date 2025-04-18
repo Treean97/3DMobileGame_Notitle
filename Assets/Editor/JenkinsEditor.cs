9274758575e2f4ebc8790cc3a22a9982aff12458
Weminsoo <weminsoo@naver.com>
2025-04-19 01:16:20 +0900

    젠킨스 에디터 수정
    PC -> Mobile 플랫폼 변경

--- Assets/Editor/JenkinsEditor.cs
+++ Assets/Editor/JenkinsEditor.cs
@@ -1,25 +1,33 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using System.Collections.Generic;
 
-class JenkinsEditor
+class MyEditorScript
 {
-    static string[] SCENES = FindEnabledEditorScenes();
-
-    static void PerformBuild()
+    [MenuItem("Build/Perform Android Build")]
+    static void PerformAndroidBuild()
     {
-        BuildPipeline.BuildPlayer(FindEnabledEditorScenes(), "Builds/Windows/MyGame.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+        // 1) Android를 활성 빌드 타겟으로 전환 (선택 사항이지만 권장)
+        EditorUserBuildSettings.SwitchActiveBuildTarget(
+            BuildTargetGroup.Android, BuildTarget.Android);
+
+        // 2) 빌드할 씬 목록 가져오기
+        string[] scenes = FindEnabledEditorScenes();
+
+        // 3) APK로 빌드
+        BuildPipeline.BuildPlayer(
+            scenes,
+            "Builds/Android/MyGame.apk",
+            BuildTarget.Android,
+            BuildOptions.None);
     }
+
     private static string[] FindEnabledEditorScenes()
     {
-        List<string> EditorScenes = new List<string>();
-        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
-        {
-            if (!scene.enabled) continue;
-            EditorScenes.Add(scene.path);
-        }
-        return EditorScenes.ToArray();
+        var editorScenes = new List<string>();
+        foreach (var scene in EditorBuildSettings.scenes)
+            if (scene.enabled)
+                editorScenes.Add(scene.path);
+        return editorScenes.ToArray();
     }
 }
